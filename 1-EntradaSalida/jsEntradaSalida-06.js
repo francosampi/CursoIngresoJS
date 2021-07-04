0f2e71811704a9719c1b30448e774a16cf5d658a
Franco Sampietro <Franco Sampietro>
2021-07-04 18:39:21 -0300

    tarea completada

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,12 +4,10 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	let num1, num2, resultado;
+	let num1, num2;
 
 	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
 	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
 
 	alert("La suma es "+parseInt(num1+num2));
-
-	console.log(num1)
 }
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -3,23 +3,43 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
 function sumar()
 {	
-	alert("ok");	
+	let num1, num2;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	alert("La suma es "+parseInt(num1+num2));
 }
 
 function restar()
 {
-	
+	let num1, num2;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	alert("La resta es "+parseInt(num1-num2));
 }
 
 function multiplicar()
 { 
-	
+	let num1, num2;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+   
+	alert("El producto es "+parseInt(num1*num2));
 }
 
 function dividir()
 {
-	
-}
+	let num1, num2;
 
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	alert("El cociente es "+parseInt(num1/num2));
+}

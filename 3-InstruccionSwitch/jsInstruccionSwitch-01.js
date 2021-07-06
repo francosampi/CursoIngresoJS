b51375ad7df496bfae4033b2fa5ad51982dc6b40
Franco Sampietro <Franco Sampietro>
2021-07-06 08:58:06 -0300

    tarea completada

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,18 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-
-
+	switch(document.getElementById("txtIdMes").value){
+		case "Enero":
+			alert("que comiences bien el año!");
+		break;
+		case "Marzo":
+			alert("a clases!!!");
+		break;
+		case "Julio":
+			alert("se vienen las vacaciones!!!");
+		break;
+		case "Diciembre":
+			alert("Felices fiestas!!!");
+		break;
+	}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,10 +1,11 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
-	
-	
-
-
+	switch(document.getElementById("txtIdMes").value){
+		case "Febrero":
+			alert("Este mes no tiene más de 29 días");
+		break;
+		default:
+			alert("Este més tiene 30 días o mas");
+		break;
+	}
 }//FIN DE LA FUNCIÓN

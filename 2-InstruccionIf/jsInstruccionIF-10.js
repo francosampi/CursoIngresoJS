3479c6606dd0255e495a58c3394dec74b443855e
Franco Sampietro <Franco Sampietro>
2021-07-13 13:20:42 -0300

    tarea completada

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -5,10 +5,16 @@ function mostrar()
 	alert("Nota: "+nota);
 
 	if (nota==9 || nota==10)
+	{
 		alert("EXCELENTE");
+	}
 	else if (nota>=4)
+	{
 		alert("APROBÓ");
+	}
 	else if (nota<4)
+	{
 		alert("Vamos, la próxima se puede");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -10,5 +10,83 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+    let precioLampara;
+ 	let cantidadLamparas;
+    let marcaLampara;
+    let precioConDescuento;
+
+    let impuestoIIBBPorcentaje=0.1;
+    let impuestoIIBB;
+
+    let descuentoLampara;
+
+    precioLampara = 35;
+
+    cantidadLamparas = document.getElementById("txtIdCantidad").value;
+    cantidadLamparas = parseInt(cantidadLamparas);
+
+    marcaLampara = document.getElementById("Marca").value;
+
+    impuestoIngresosBrutos = 0.1;
+
+    if (cantidadLamparas>=6)
+    {
+        descuentoLampara = 0.5;
+    }
+    else
+    {
+        if (cantidadLamparas==5)
+        {
+            if (marcaLampara=="ArgentinaLuz")
+            {
+                descuentoLampara = 0.4;
+            }
+            else
+            {
+                descuentoLampara = 0.4;
+            }
+        }
+        if (cantidadLamparas==4)
+        {
+            if (marcaLampara=="ArgentinaLuz" || marcaLampara=="FelipeLamparas")
+            {
+                descuentoLampara = 0.25;
+            }
+            else
+            {
+                descuentoLampara = 0.2;
+            }
+        }
+        else
+        {
+            if (marcaLampara=="ArgentinaLuz")
+            {
+                descuentoLampara = 0.15;
+            }
+            else
+            {
+                if (marcaLampara=="FelipeLamparas")
+                {
+                    descuentoLampara = 0.1;
+                }
+                else
+                {
+                    descuentoLampara = 0.05;
+                }
+            }
+        }
+    }
+    precioConDescuento=(cantidadLamparas*precioLampara);
+    precioConDescuento=precioConDescuento*descuentoLampara;
+
+    if (precioConDescuento>120)
+    {
+        impuestoIIBB=precioConDescuento*impuestoIIBBPorcentaje;
+
+        precioConDescuento=precioConDescuento+impuestoIIBB;
+
+        alert("IIBB Usted pagó "+impuestoIIBB);
+    }
+
+    document.getElementById("txtIdprecioDescuento").value = precioConDescuento;
 }
